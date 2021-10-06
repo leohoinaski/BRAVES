26d3cd2fcd3995bb77ed6fbe5b1f44af85214618
leohoinaski <62803888+leohoinaski@users.noreply.github.com>
2021-10-06 11:30:13 -0300

    Atualização do código

--- BRAVESdatabase_main.py
+++ BRAVESdatabase_main.py
@@ -54,11 +54,15 @@ Outputs:
     
     BRAVESannualEmiss_ netCDF files
     
+    BRAVESdatabase2CMAQ_ ...nc
+    
+    BRAVESdatabaseTempEmiss_ ...nc
     
     
     
 External functions:
-    roadDensity_v1, roadEmiss_v1, mergeRoadEmiss_v1, BRAVES2netCDF_v1
+    roadDensity_v1, roadEmiss_v1, mergeRoadEmiss_v1, BRAVES2netCDF_v1,
+    BRAVES_temporalDisag, createNETCDFtemporalfromNC, createNETCDFtemporalBySpecies
     
 
 Last update = 30/09/2021
@@ -77,48 +81,74 @@ import os
 import numpy as np
 import pandas as pd
 
-#%%================================INPUTS========================================
+#%%================================INPUTS======================================
+
+#------------------------------Setting folders---------------------------------
 
 # Path to functions and BRAVESdatabase_main.py
 rootPath = "/media/leohoinaski/HDD/BRAVES_database"
+
 # Defining folder path with openstreetmaps folder
 dirPath = "/media/leohoinaski/HDD/BRAVES_database/Shapefiles"
+
 # Path to folder with roadLenght....csv
 outPath = "/media/leohoinaski/HDD/BRAVES_database/Outputs"
+
 # Path to folder with BRAVES outputs'
 bravesPath = "/media/leohoinaski/HDD/BRAVES_database/BRAVESoutputs"
+
 # Path to folder with chemical speciation profiles
 folderSpec = '/media/leohoinaski/HDD/BRAVES_database/ChemicalSpec'
 
-# Defining initial and final latitude and longitude. Brazilian borders by defaut
+# Road file name - use gis_osm for states other than SC
+roadFileName = 'SC_ROADS.shp' # for SC state
+#roadFileName = 'gis_osm_roads_free_1.shp'
+
+#-------------------------Setting grid resolution------------------------------
+
 # Users can change the domain and resolution here.
 lati =-30 #lati = int(round(bound.miny)) # Initial latitude (lower-left)
+
 latf = -24 #latf = int(round(bound.maxy)) # Final latitude (upper-right)
+
 loni = -54 #loni = int(round(bound.minx)) # Initial longitude (lower-left)
+
 lonf = -47 #lonf = int(round(bound.maxx)) # Final longitude (upper-right)
-deltaX = 0.05 # Grid resolution/spacing in x direction
-deltaY = 0.05 # Grig resolution/spacing in y direction
-IBGE_CODES = [42]
+
+deltaX = 0.01 # Grid resolution/spacing in x direction
+
+deltaY = 0.01 # Grig resolution/spacing in y direction
+
+#---------------------------Vehicular emissions--------------------------------
+
+IBGE_CODES = [42] # include the IBGE code from the states to be considered
+
+
+#---------------------------- Time window--------------------------------------
+
 years=[2013]
-roadFileName = 'SC_ROADS.shp'
-#roadFileName = 'gis_osm_roads_free_1.shp'
-fileId = 'SC' # Code to identify your output files
 
-roadDensPrefix = str(deltaX)+'x'+str(deltaY)
-
-
-runOrnotTempFiles = 1 # 0 for no and 1 for yes
-spec = 'SO2'
-runOrnotCMAQemiss = 1 # 0 for no and 1 for yes
-# Define the files to disaggregate
-files = ['BRAVESdatabaseAnnual_SC_ComLight_2013.nc']
-year=2013
 months = [1] # Set the month of your simulation
+
 days = [1,2] # Set the day of your simulation
 
-#%%==============================================================================
 
-#----------------------------- PROCESSING--------------------------------------
+#---------------------------Outputs definition--------------------------------
+
+fileId = 'SC' # Code to identify your output files
+
+roadDensPrefix = str(deltaX)+'x'+str(deltaY) # grid definition identification
+
+# Create disaggregated files - temporal, spatial, and one specie
+runOrnotTempFiles = 1 # 0 for no and 1 for yes
+specs = ['SO2']  # Identification of the specie 
+
+# Create CMAQ emission inputs - temporal, spatial, and all species
+runOrnotCMAQemiss = 1 # 0 for no and 1 for yes
+files = ['BRAVESdatabaseAnnual_SC_ComLight_2013.nc'] # Define the files to disaggregate
+
+
+#%%============================PROCESSING========================================
 
 # cd to the main folder
 os.chdir(rootPath)
@@ -143,26 +173,29 @@ BRAVES2netCDF(outPath,folderSpec,outPath,years,fileId)
 # Creating input files for CMAQ
 if runOrnotCMAQemiss==1:
     for file in files:
-        for month in months:
-            for day in days:
-                dataTempo,xv,yv,lat,lon,center,disvec,prefix=BRAVES_temporalDisag(rootPath,outPath,file,fileId,month,day)
-                for jj in np.unique(disvec.day):       
-                    name = 'BRAVESdatabase2CMAQ_'+fileId+'_'+prefix+'_'+str(year)+'_'+str(month)+'_'+str(jj)+'.nc'
-                    dayT = np.where(disvec.day==jj)
-                    createNETCDFtemporalfromNC(outPath,name,dataTempo,xv,yv,lat,lon,center,disvec,month)
+        for year in years:
+            for month in months:
+                for day in days:
+                    dataTempo,xv,yv,lat,lon,center,disvec,prefix=BRAVES_temporalDisag(rootPath,outPath,file,fileId,month,day)
+                    for jj in np.unique(disvec.day):       
+                        name = 'BRAVESdatabase2CMAQ_'+fileId+'_'+prefix+'_'+str(year)+'_'+str(month)+'_'+str(jj)+'.nc'
+                        dayT = np.where(disvec.day==jj)
+                        createNETCDFtemporalfromNC(outPath,name,dataTempo,xv,yv,lat,lon,center,disvec,month)
 
-
+# Creating temporal files for one specie
 if runOrnotTempFiles==1:
-    for file in files:
-        for month in months:
-            for day in days:
-                file_path = 'CMAQ_speciesMW.csv'
-                smm = pd.read_csv(folderSpec+'/'+file_path)
-                specIdx=smm[smm.ID==spec].index.to_numpy()[0]
-                dataTempo,xv,yv,lat,lon,center,disvec,prefix=BRAVES_temporalDisag(rootPath,outPath,file,fileId,month,day)
-                for jj in np.unique(disvec.day):       
-                    name = 'BRAVESdatabaseTempEmiss_'+fileId+'_'+prefix+'_'+str(year)+'_'+str(month)+'_'+str(jj)+'.nc'
-                    dayT = np.where(disvec.day==jj)
-                    createNETCDFtemporalBySpecies(outPath,name,
-                                                  dataTempo[:,specIdx,:,:].reshape((dataTempo.shape[0],1,dataTempo.shape[2],dataTempo.shape[3])),
-                                                  xv,yv,lat,lon,center,disvec,spec)
+    for spec in specs:
+        for file in files:
+            for year in years:
+                for month in months:
+                    for day in days:
+                        file_path = 'CMAQ_speciesMW.csv'
+                        smm = pd.read_csv(folderSpec+'/'+file_path)
+                        specIdx=smm[smm.ID==spec].index.to_numpy()[0]
+                        dataTempo,xv,yv,lat,lon,center,disvec,prefix=BRAVES_temporalDisag(rootPath,outPath,file,fileId,month,day)
+                        for jj in np.unique(disvec.day):       
+                            name = 'BRAVESdatabaseTempEmiss_'+fileId+'_'+prefix+'_'+str(year)+'_'+str(month)+'_'+str(jj)+'.nc'
+                            dayT = np.where(disvec.day==jj)
+                            createNETCDFtemporalBySpecies(outPath,name,
+                                                          dataTempo[:,specIdx,:,:].reshape((dataTempo.shape[0],1,dataTempo.shape[2],dataTempo.shape[3])),
+                                                          xv,yv,lat,lon,center,disvec,spec)
