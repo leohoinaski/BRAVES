1511e7f4d269ff77704dd206f7731fe45c2cbb7e
Leonardo Hoinaski <leonardo.hoinaski@ufsc.br>
2022-09-28 13:05:11 -0300

    FixingTemporaDisag

--- BRAVES_temporalDisag_v1.py
+++ BRAVES_temporalDisag_v1.py
@@ -56,58 +56,8 @@ def temporalDisagVehicular(dataNC,year,month,day,hourdis,weekdis,monthdis,ltz):
     disvec['day'] = disvec.index.day
     disvec['hour'] = disvec.index.hour
     disvec['weekday'] = disvec.index.weekday # Monday is 0 and Sunday is 6
-   # disvec['hourdis'] = numpy.matlib.repmat(
-   #     hourdis, 1, int(disvec.shape[0]/24)).transpose() 
-    disvec['hourdis']=np.zeros([disvec['hour'].shape[0],1])
-    for ii in range(0,disvec['hourdis'].shape[0]):
-        if disvec['hourdis'][ii] == 0:
-            disvec['hourdis'][ii] = hourdis[0]
-        if disvec['hourdis'][ii] == 1:
-            disvec['hourdis'][ii] = hourdis[1]
-        if disvec['hourdis'][ii] == 2:
-            disvec['hourdis'][ii] = hourdis[2]
-        if disvec['hourdis'][ii] == 3:
-            disvec['hourdis'][ii] = hourdis[3]
-        if disvec['hourdis'][ii] == 4:
-            disvec['hourdis'][ii] = hourdis[4]
-        if disvec['hourdis'][ii] == 5:
-            disvec['hourdis'][ii] = hourdis[5]
-        if disvec['hourdis'][ii] == 6:
-            disvec['hourdis'][ii] = hourdis[6]
-        if disvec['hourdis'][ii] == 7:
-            disvec['hourdis'][ii] = hourdis[7]
-        if disvec['hourdis'][ii] == 8:
-            disvec['hourdis'][ii] = hourdis[8]
-        if disvec['hourdis'][ii] == 9:
-            disvec['hourdis'][ii] = hourdis[9]
-        if disvec['hourdis'][ii] == 10:
-            disvec['hourdis'][ii] = hourdis[10]
-        if disvec['hourdis'][ii] == 11:
-            disvec['hourdis'][ii] = hourdis[11]
-        if disvec['hourdis'][ii] == 12:
-            disvec['hourdis'][ii] = hourdis[12]
-        if disvec['hourdis'][ii] == 13:
-            disvec['hourdis'][ii] = hourdis[13]
-        if disvec['hourdis'][ii] == 14:
-            disvec['hourdis'][ii] = hourdis[14]
-        if disvec['hourdis'][ii] == 15:
-            disvec['hourdis'][ii] = hourdis[15]
-        if disvec['hourdis'][ii] == 16:
-            disvec['hourdis'][ii] = hourdis[16]
-        if disvec['hourdis'][ii] == 17:
-            disvec['hourdis'][ii] = hourdis[17]
-        if disvec['hourdis'][ii] == 18:
-            disvec['hourdis'][ii] = hourdis[18]
-        if disvec['hourdis'][ii] == 19:
-            disvec['hourdis'][ii] = hourdis[19]
-        if disvec['hourdis'][ii] == 20:
-            disvec['hourdis'][ii] = hourdis[20]
-        if disvec['hourdis'][ii] == 21:
-            disvec['hourdis'][ii] = hourdis[21]
-        if disvec['hourdis'][ii] == 22:
-            disvec['hourdis'][ii] = hourdis[22]
-        if disvec['hourdis'][ii] == 23:
-            disvec['hourdis'][ii] = hourdis[23] 
+    disvec['hourdis'] = numpy.matlib.repmat(
+        hourdis, 1, int(disvec.shape[0]/24)).transpose() 
    
     disvec['weekdis']=np.zeros([disvec['weekday'].shape[0],1])
     for ii in range(0,disvec['weekday'].shape[0]):
@@ -261,7 +211,7 @@ def BRAVES_temporalDisagMCIP(rootPath,outPath,file,time):
         print(dataVar[ii])
         dataNC =np.concatenate((dataNC,dataloop), axis=1)
     
-    # # Informas das grades 
+    # # Informações das grades 
     # xi = ds.getncattr('XORIG')
     # yi = ds.getncattr('YORIG')
     # dx = ds.getncattr('XCELL')
@@ -304,7 +254,7 @@ def temporalDisagVehicularMCIP(dataNC,time,hourdis,weekdis,monthdis,ltz,hours):
         int(dt0.year), int(dt0.month), int(dt0.day), int(hours[0]), 0)
     endDate = datetime.datetime(
         int(dt1.year), int(dt1.month), int(dt1.day), int(hours[1]), 0)
-    datePfct = np.arange(np.datetime64(startDate),np.datetime64(endDate)+3600000000,3600000000)
+    datePfct = np.arange(np.datetime64(startDate),np.datetime64(endDate),3600000000)
     numWeeks = datePfct.shape[0]/(7*24) # Number of weeks
     disvec = pd.DataFrame()
     disvec = disvec.reindex(datePfct, fill_value=np.nan)
@@ -313,59 +263,9 @@ def temporalDisagVehicularMCIP(dataNC,time,hourdis,weekdis,monthdis,ltz,hours):
     disvec['day'] = disvec.index.day
     disvec['hour'] = disvec.index.hour
     disvec['weekday'] = disvec.index.weekday # Monday is 0 and Sunday is 6
-#    disvec['hourdis'] = numpy.matlib.repmat(
- #       hourdis, 1, int(disvec.shape[0]/24)).transpose() 
-    disvec['hourdis']=np.zeros([disvec['hour'].shape[0],1])
-    for ii in range(0,disvec['hourdis'].shape[0]):
-        if disvec['hourdis'][ii] == 0:
-            disvec['hourdis'][ii] = hourdis[0]
-        if disvec['hourdis'][ii] == 1:
-            disvec['hourdis'][ii] = hourdis[1]
-        if disvec['hourdis'][ii] == 2:
-            disvec['hourdis'][ii] = hourdis[2]
-        if disvec['hourdis'][ii] == 3:
-            disvec['hourdis'][ii] = hourdis[3]
-        if disvec['hourdis'][ii] == 4:
-            disvec['hourdis'][ii] = hourdis[4]
-        if disvec['hourdis'][ii] == 5:
-            disvec['hourdis'][ii] = hourdis[5]
-        if disvec['hourdis'][ii] == 6:
-            disvec['hourdis'][ii] = hourdis[6]
-        if disvec['hourdis'][ii] == 7:
-            disvec['hourdis'][ii] = hourdis[7]
-        if disvec['hourdis'][ii] == 8:
-            disvec['hourdis'][ii] = hourdis[8]
-        if disvec['hourdis'][ii] == 9:
-            disvec['hourdis'][ii] = hourdis[9]
-        if disvec['hourdis'][ii] == 10:
-            disvec['hourdis'][ii] = hourdis[10]
-        if disvec['hourdis'][ii] == 11:
-            disvec['hourdis'][ii] = hourdis[11]
-        if disvec['hourdis'][ii] == 12:
-            disvec['hourdis'][ii] = hourdis[12]
-        if disvec['hourdis'][ii] == 13:
-            disvec['hourdis'][ii] = hourdis[13]
-        if disvec['hourdis'][ii] == 14:
-            disvec['hourdis'][ii] = hourdis[14]
-        if disvec['hourdis'][ii] == 15:
-            disvec['hourdis'][ii] = hourdis[15]
-        if disvec['hourdis'][ii] == 16:
-            disvec['hourdis'][ii] = hourdis[16]
-        if disvec['hourdis'][ii] == 17:
-            disvec['hourdis'][ii] = hourdis[17]
-        if disvec['hourdis'][ii] == 18:
-            disvec['hourdis'][ii] = hourdis[18]
-        if disvec['hourdis'][ii] == 19:
-            disvec['hourdis'][ii] = hourdis[19]
-        if disvec['hourdis'][ii] == 20:
-            disvec['hourdis'][ii] = hourdis[20]
-        if disvec['hourdis'][ii] == 21:
-            disvec['hourdis'][ii] = hourdis[21]
-        if disvec['hourdis'][ii] == 22:
-            disvec['hourdis'][ii] = hourdis[22]
-        if disvec['hourdis'][ii] == 23:
-            disvec['hourdis'][ii] = hourdis[23] 
-
+    disvec['hourdis'] = numpy.matlib.repmat(
+        hourdis, 1, int(disvec.shape[0]/24)).transpose() 
+   
     disvec['weekdis']=np.zeros([disvec['weekday'].shape[0],1])
     for ii in range(0,disvec['weekday'].shape[0]):
         if disvec['weekday'][ii] == 6:
